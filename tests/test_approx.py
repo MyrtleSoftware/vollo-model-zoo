9a329e80006f61319db3a7a4ea25a5513f596462
Conor <conor@myrtle.com>
2026-03-05 12:11:58 +0000

    full accuracy test

--- tests/test_approx.py
+++ tests/test_approx.py
@@ -26,50 +26,59 @@ def fp32sig(x):
     with Fp32Activations():
         denom = 1.0 + torch.exp(-x)
 
-    # Now we want to compute 1 / denom
-
-    y0 = 1 / denom  # bf16
+    # Now we want to compute 1 / denom, this is a bf16 approx
+    y = 1 / denom
 
     with Fp32Activations():
         # do newton-raphson to compute 1 / denom in fp32
-        y1 = y0 * (2 - denom * y0)
-        return y1
+        for _ in range(2):
+            y = y * (2 - denom * y)
+
+        return y
 
 
 def test_meta():
     x = gen_all_bf16().to(torch.float32)
 
-    # Get positive values only
-    x = x[x > 0]
+    # Get region of interest (saturation of sigmoid)
+    b = 200
+    x = x[-b < x]
+    x = x[x < b]
 
     y_ref_f32 = torch.sigmoid(x)
     y_ref_b16 = y_ref_f32.to(torch.bfloat16).to(torch.float32)
-    y_vol_b16 = vollo_fn(torch.sigmoid, "V80")(x).to(torch.float32)
 
     assert y_ref_f32.isfinite().all()
     assert y_ref_b16.isfinite().all()
-    assert y_vol_b16.isfinite().all()
 
     ref_max = (y_ref_f32 - y_ref_b16).abs().max()
-    ref_avg = (y_ref_f32 - y_ref_b16).abs().mean()
+    ref_avg = (y_ref_f32 - y_ref_b16).abs().sum()
 
     print(f"Intrinsic max error: {ref_max:.5e}")
     print(f"Intrinsic avg error: {ref_avg:.5e}")
 
+    # === bf16 sigmoid
+
+    y_vol_b16 = vollo_fn(torch.sigmoid, "V80")(x).to(torch.float32)
+
+    assert y_vol_b16.isfinite().all()
+
     vollo_max = (y_ref_f32 - y_vol_b16).abs().max()
-    vollo_avg = (y_ref_f32 - y_vol_b16).abs().mean()
+    vollo_avg = (y_ref_f32 - y_vol_b16).abs().sum()
 
     print(f"Vollo max error: {vollo_max:.5e}")
     print(f"Vollo avg error: {vollo_avg:.5e}")
 
+    # === fp32 sigmoid
+
+    y_vol_fp32 = vollo_fn(fp32sig, "V80")(x).to(torch.float32)
+
+    assert y_vol_fp32.isfinite().all()
+
+    vollo_fp32_max = (y_ref_f32 - y_vol_fp32).abs().max()
+    vollo_fp32_avg = (y_ref_f32 - y_vol_fp32).abs().sum()
+
+    print(f"Vollo fp32 max error: {vollo_fp32_max:.5e}")
+    print(f"Vollo fp32 avg error: {vollo_fp32_avg:.5e}")
+
     assert False, "ok"
-
-    plt.plot(x, y_ref_f32.numpy(), label="Ref-f32")
-    plt.plot(x, y_ref_b16.numpy(), label="Ref-b16")
-    plt.plot(x, y_vol_b16.numpy(), label="Vollo")
-
-    plt.xlim(-10, -3)
-    plt.ylim(0, 0.02)
-
-    plt.legend()
-    plt.savefig("/home/conor/Downloads/f16.png")
