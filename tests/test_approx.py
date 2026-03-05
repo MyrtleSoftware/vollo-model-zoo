2e85720f64c09bad7ae522f265ae94419be8fba2
Conor <conor@myrtle.com>
2026-03-05 14:59:06 +0000

    compare Lut approximation

--- tests/test_approx.py
+++ tests/test_approx.py
@@ -2,7 +2,7 @@ import torch
 from beartype import beartype
 from vollo_torch import Fp32Activations
 
-from vollo_model_zoo.approx import round_mantisa
+from vollo_model_zoo.approx import round_mantisa, sigmoid
 from vollo_model_zoo.vm import vollo_fn
 
 
@@ -46,11 +46,21 @@ def test_meta():
 
     assert y_ref_f32.isfinite().all()
 
-    for n in [16]:
+    for n in [16, 26]:
         y_ref_bn = round_mantisa(y_ref_f32, n=n - 9)
         ref_max = (y_ref_f32 - y_ref_bn).abs().max()
         ref_avg = (y_ref_f32 - y_ref_bn).abs().sum()
         print(f"Ideal bf{n} error: {ref_max:.5e}, {ref_avg:.5e}")
+
+    # === The python table approximation
+
+    y_vol_approx = sigmoid(x)
+
+    assert y_vol_approx.isfinite().all()
+
+    approx_max = (y_ref_f32 - y_vol_approx).abs().max()
+    approx_avg = (y_ref_f32 - y_vol_approx).abs().sum()
+    print(f"LuT'd bf16 error: {approx_max:.5e}, {approx_avg:.5e}")
 
     # === bf16 sigmoid
 
@@ -60,25 +70,26 @@ def test_meta():
 
     vollo_max = (y_ref_f32 - y_vol_b16).abs().max()
     vollo_avg = (y_ref_f32 - y_vol_b16).abs().sum()
-
     print(f"Vollo bf16 error: {vollo_max:.5e}, {vollo_avg:.5e}")
 
     # === fp32 sigmoid
 
     y_vol_f32 = vollo_fn(fp32sig, "V80")(x).to(torch.float32)
 
-    for i, rew in enumerate(y_vol_f32.view(64, -1)):
-        print(i, rew.isfinite().all(), rew[0], rew[-1])
-
-    non_finite_indices = ~y_vol_f32.isfinite()
-    print(f"nun nans {non_finite_indices.sum()}")
-    # Get indices of non-finite values
-
     assert y_vol_f32.isfinite().all()
 
     vollo_fp32_max = (y_ref_f32 - y_vol_f32).abs().max()
     vollo_fp32_avg = (y_ref_f32 - y_vol_f32).abs().sum()
-
     print(f"Vollo fp32 error: {vollo_fp32_max:.5e}, {vollo_fp32_avg:.5e}")
 
-    assert False, "ok"
+    # === Final comparison
+
+    max_improvement = (vollo_max / vollo_fp32_max).log2()
+    avg_improvement = (vollo_avg / vollo_fp32_avg).log2()
+    print(f"Max improvement: {max_improvement:.2f} bits")
+    print(f"Avg improvement: {avg_improvement:.2f} bits")
+
+    assert avg_improvement > 9.5
+    assert max_improvement > 10.5
+
+    assert False, "OK"
