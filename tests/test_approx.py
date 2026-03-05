e0213f1a1e76e1176aaed88e53dcbba22b763439
Conor <conor@myrtle.com>
2026-03-05 14:45:51 +0000

    overflow protection

--- tests/test_approx.py
+++ tests/test_approx.py
@@ -1,5 +1,6 @@
 import torch
 from beartype import beartype
+from vollo_torch import Fp32Activations
 
 from vollo_model_zoo.approx import round_mantisa
 from vollo_model_zoo.vm import vollo_fn
@@ -19,19 +20,19 @@ def gen_all_bf16() -> torch.Tensor:
     return bf16s.sort()[0]
 
 
-from matplotlib import pyplot as plt
-from vollo_torch import Fp32Activations
-
-
 def fp32sig(x):
+    # Prevent exp -> inf overflow
+    x = torch.clamp(x, min=-20)
+    x = -x
+
     with Fp32Activations():
-        denom = 1.0 + torch.exp(-x)
+        denom = 1.0 + torch.exp(x)
 
     # Now we want to compute 1 / denom, this is a bf16 approx
     y = 1 / denom
 
     with Fp32Activations():
-        # do newton-raphson to compute 1 / denom in fp32
+        # Newton-Raphson to compute 1 / denom in fp32
         for _ in range(2):
             y = y * (2 - denom * y)
 
@@ -40,11 +41,6 @@ def fp32sig(x):
 
 def test_meta():
     x = gen_all_bf16().to(torch.float32)
-
-    # Get region of interest (saturation of sigmoid)
-    b = 20
-    x = x[-b < x]
-    x = x[x < b]
 
     y_ref_f32 = torch.sigmoid(x)
 
@@ -69,12 +65,19 @@ def test_meta():
 
     # === fp32 sigmoid
 
-    y_vol_fp32 = vollo_fn(fp32sig, "V80")(x).to(torch.float32)
+    y_vol_f32 = vollo_fn(fp32sig, "V80")(x).to(torch.float32)
 
-    assert y_vol_fp32.isfinite().all()
+    for i, rew in enumerate(y_vol_f32.view(64, -1)):
+        print(i, rew.isfinite().all(), rew[0], rew[-1])
 
-    vollo_fp32_max = (y_ref_f32 - y_vol_fp32).abs().max()
-    vollo_fp32_avg = (y_ref_f32 - y_vol_fp32).abs().sum()
+    non_finite_indices = ~y_vol_f32.isfinite()
+    print(f"nun nans {non_finite_indices.sum()}")
+    # Get indices of non-finite values
+
+    assert y_vol_f32.isfinite().all()
+
+    vollo_fp32_max = (y_ref_f32 - y_vol_f32).abs().max()
+    vollo_fp32_avg = (y_ref_f32 - y_vol_f32).abs().sum()
 
     print(f"Vollo fp32 error: {vollo_fp32_max:.5e}, {vollo_fp32_avg:.5e}")
 
