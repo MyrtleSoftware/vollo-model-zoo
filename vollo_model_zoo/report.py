4ccc6ff333598abe4e958cfcd15a9cc9e48e13ef
Conor <conor@myrtle.com>
2026-03-24 16:11:33 +0000

    further report option

---
+++ vollo_model_zoo/report.py
@@ -0,0 +1,87 @@
+import argparse
+from itertools import groupby
+from pathlib import Path
+
+from beartype import beartype
+
+from vollo_model_zoo.parse import parse_records_from_json
+
+
+@beartype
+def main() -> int:
+    """
+    Entry point for 'report' command.
+    """
+    parser = argparse.ArgumentParser(description="Generate report from benchmarks")
+
+    parser.add_argument(
+        "input",
+        type=Path,
+        help="JSON input file",
+    )
+
+    mk_default_output = "./benchmarks/README.md"
+    parser.add_argument(
+        "--output",
+        type=Path,
+        default=mk_default_output,
+        help=f"Markdown output file for summary table (default: {mk_default_output})",
+    )
+
+    args = parser.parse_args()
+
+    return _generate_report(args.input, args.output)
+
+
+@beartype
+def _generate_report(input: Path, output: Path) -> int:
+    """
+    Generate performance plots from benchmark JSON files.
+    """
+    records = parse_records_from_json(input)
+
+    assert records, "No valid benchmark results found."
+
+    versions = [x["version"] for x in records]
+
+    assert all(
+        v == versions[0] for v in versions
+    ), "Multiple versions found in input JSON"
+
+    def key_func(x):
+        return (x["config"], x["model"])
+
+    records.sort(key=key_func, reverse=True)
+
+    with open(output, "w") as f:
+
+        def write(x):
+            print(x, file=f)
+
+        # Format as markdown
+        write(f"#Vollo Model Zoo Benchmarks (version {versions[0]})")
+        write("")
+        write("Compute latency for an approximately 1-million parameter model.")
+
+        for config, config_group in groupby(records, key=lambda x: x["config"]):
+            write("")
+            write(f"## Configuration: {config}")
+            write("")
+            write("| Model | Latency/us | Latency/us (contiguous) | Metadata |")
+            write("| ----- | ---------- | ----------------------- | -------- |")
+
+            for model, group in groupby(config_group, key=lambda x: x["model"]):
+                # The one closest to 1-mil parameters
+                chosen = min(group, key=lambda x: abs(x["param_count"] - 1_000_000))
+
+                l1 = chosen["latency_spaced"]
+                l2 = chosen["latency_contiguous"]
+                mt = chosen["meta"]
+
+                print(f"| {model} | {l1:.2f} | {l2:.2f} | {mt} |", file=f)
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
